67185873dd8205f39ff46581dbcbbf5bf34b6ae3
eldhoolickal <eldhoolickal@gmail.com>
2026-03-15 14:22:36 +0530

    Add files via upload
    Initial commit

---
+++ src/test/java/com/example/demo/springboot_h2_rest_api/SpringbootH2RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.springboot_h2_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootH2RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
